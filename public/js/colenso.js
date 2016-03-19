1c145774d4603f18fa2cccdc3809da54996dd099
semillkasz <Kaszandra@MacBook-Pro-2.local>
2016-03-19 14:26:51 +1300

    The Colenso Project
    Initial Commit
    - html
    - images
    - css

---
+++ public/js/colenso.js
@@ -0,0 +1,18 @@
+
+
+function goHome(){
+	window.open("index.html","_self");
+}
+function browse(){
+	window.open("browse.html","_self");
+}
+function addLetter(){
+	window.open("add.html","_self");
+}
+function searchLetter(){
+	window.open("search.html","_self");
+}
+
+function displayTitles(){
+	
+}
