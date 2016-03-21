d6c947e69d922d70409587ae4af421af0be80177
semillkasz <Kaszandra@MacBook-Pro-2.local>
2016-03-22 00:09:23 +1300

    - navbar update - libraries were added to package.son

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var basex = require('basex');
+var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
